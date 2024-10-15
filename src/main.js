3c9ac0e8fa0853cad79809867211d479db867f2e
Franchesca Boongaling <fboongal@ucsc.edu>
2024-10-15 14:12:34 -0700

    added temporary title text

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ let config = {
             //debug: true
         }
     },
+    pixelArt: true,
     scene: [ Menu ]
 }
 
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,9 +9,25 @@ class Menu extends Phaser.Scene {
 
     create(){
 
+        // display menu
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        // display menu text
+        this.add.text(centerX, centerY, 'Ye Guild Clerk!', menuConfig).setOrigin(0.5)
     }
 
     update(){
-        
+
     }
 }
